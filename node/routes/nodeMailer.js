44932aa8156db0481beabc4406f7342504f1d488
Cosmas <24183871+Cosmas6@users.noreply.github.com>
2022-03-07 13:01:49 +0300

    Nodemailer Working

--- node/routes/nodeMailer.js
+++ node/routes/nodeMailer.js
@@ -1,18 +1,37 @@
 var express = require("express");
 var router = express.Router();
+require("dotenv").config();
 var nodemailer = require("nodemailer");
-var app = express();
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     type: "OAuth2",
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+    clientId:
+    process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken:
+    process.env.REFRESH_TOKEN,
   },
+});
+
+let mailOptions = {
+  from: "cosmasmusis@gmail.com",
+  to: "cosmasmusis@gmail.com",
+  subject: "Nodemailer Project",
+  text: "Hi from your nodemailer project",
+};
+
+transporter.sendMail(mailOptions, function (err, data) {
+  if (err) {
+    console.log("Error " + err);
+  } else {
+    console.log("Email sent successfully");
+  }
 });
 
 module.exports = router;
