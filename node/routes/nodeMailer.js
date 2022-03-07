d615637ed65772f2de4b793c2f8e08e0614b1ffd
Cosmas <24183871+Cosmas6@users.noreply.github.com>
2022-03-07 13:36:27 +0300

    File attachment success in node

--- node/routes/nodeMailer.js
+++ node/routes/nodeMailer.js
@@ -5,33 +5,59 @@ var nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-    clientId:
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
     process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken:
-    process.env.REFRESH_TOKEN,
-  },
-});
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
 
-let mailOptions = {
-  from: "cosmasmusis@gmail.com",
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+    },
+  });
+
+  return transporter;
+};
+
+const sendEmail = async (emailOptions) => {
+  let emailTransporter = await createTransporter();
+  await emailTransporter.sendMail(emailOptions);
+};
+
+sendEmail({
+  from: process.env.EMAIL,
   to: "cosmasmusis@gmail.com",
   subject: "Nodemailer Project",
   text: "Hi from your nodemailer project",
-};
-
-transporter.sendMail(mailOptions, function (err, data) {
-  if (err) {
-    console.log("Error " + err);
-  } else {
-    console.log("Email sent successfully");
-  }
+  attachments: [
+    {
+      // file on disk as an attachment
+      filename: "text3.txt",
+      content: "hello world!",
+    },
+  ],
 });
 
 module.exports = router;
