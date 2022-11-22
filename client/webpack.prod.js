89b09d30101c01a8a1bdc3a123afdb4922994823
Cosmas6 <cosmasmusis@gmail.com>
2022-11-22 12:55:29 +0300

    Edit

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -4,14 +4,20 @@ const CompressionPlugin = require("compression-webpack-plugin");
 const mode = "production";
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(common, {
   mode: mode,
   devtool: "source-map",
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [
+      new TerserPlugin(),
+      new UglifyJsPlugin({ test: /\.js(\?.*)?$/i }),
+    ],
+
     splitChunks: {
+      chunks: "async",
       cacheGroups: {
         styles: {
           name: "styles",
